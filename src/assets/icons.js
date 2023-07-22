42455e566e80a6732335532c5749b5a85b4a8710
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-22 10:07:04 +0545

    Icons from Heroicons

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,18 +1,6 @@
 export const CartIcon = () => {
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth={2}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9
-      9 0 015.636 5.636m12.728 12.728L5.636 5.636"
-    />
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
 </svg>
 };
 
