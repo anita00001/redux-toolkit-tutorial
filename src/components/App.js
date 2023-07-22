f6012f04a735eb7b1a65a87a6d6b5259f1eab07a
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-22 09:29:20 +0545

    useSelector hook to access the store

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,9 @@ import '../styles/App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <main className="App-header">
         <NavBar />
-      </header>
+      </main>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,26 @@
-import {cartIcon} from '../assets/icons';
+// import {CartIcon} from '../assets/icons';
 import {useSelector} from 'react-redux';
 
 const NavBar = () => {
+  console.log(
+    useSelector((store)=>{
+      console.log(store)
+    })
+  );
+
   return (
     <nav>
       <div className="nav-center">
         <h3>Redux Toolkit</h3>
       </div>
+      <div className="nav-container">
+        {/* <CartIcon /> */}
+      </div>
+      <div className="amount-container">
+        <p className="total-amount">0</p>
+      </div>
     </nav>
   );
-};
+}
 
 export default NavBar;
