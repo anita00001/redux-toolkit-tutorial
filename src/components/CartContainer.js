234093120351d045c11fa343b9ce0bee12d1b95c
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-22 11:21:43 +0545

    Render array of objects

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,18 +1,29 @@
 import React from 'react'
-import cartItem from '../redux/cart/cartSlice'
+import CartItem from './CartItem'
 import { useSelector } from 'react-redux'
 
 const CartContainer = () => {
-    const {cartItem, total, amount} = useSelector((store) => store.cart);
+    const {cartItems, total, amount} = useSelector((store) => store.cart);
     
     if(amount < 1) {
-        return <section>
+        return <section className='cart'>
             <header>
                 <h2>your bag</h2>
                 <h4 className="empty-cart">is currently empty</h4>
             </header>
         </section>
     }
+
+    return <section className='cart'>
+        <header>
+            <h2>your bag</h2>
+        </header>
+        <div>
+            {cartItems.map((item) => {
+                return <CartItem key={item.id} {...item} />
+            })}
+        </div>
+    </section>
 }
 
 export default CartContainer
