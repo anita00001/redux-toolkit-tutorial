701485e2a64ef84aab6032834fdb025878c482ed
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-22 09:14:47 +0545

    Create a Navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import {cartIcon} from '../assets/icons';
+import {useSelector} from 'react-redux';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>Redux Toolkit</h3>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
