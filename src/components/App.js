6e3c6d2a76fa4a3f4ce4db13e270692044a29b84
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-22 12:57:07 +0545

    Render calculated total and number of items on the cart

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,19 @@
+import React from 'react';
 import NavBar from './NavBar';
 import CartContainer from './CartContainer';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from '../redux/cart/cartSlice';
+import { useEffect } from 'react';
 import '../styles/App.css';
 
 function App() {
+  const {cartItems} = useSelector((store)=>store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(calculateTotals())
+  }, [cartItems])
+
   return (
     <div className="App">
       <main className="App-header">
