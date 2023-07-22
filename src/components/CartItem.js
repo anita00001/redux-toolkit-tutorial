16f2309d85f524cbbcc69263dce564922d737ff4
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-22 11:58:05 +0545

    add styling

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import {ChevronDown, ChevronUp} from '../assets/icons'
 
-function CartItem() {
+const CartItem = ({id, name, price, amount, image}) => {
   return (
-    <div>CartItem</div>
+    <article className='cart-item'>
+      <img src={image} alt={name} />
+      <div>
+        <h4>{name}</h4>
+        <h5 className='item-price'>{price}</h5>
+        <button className='remove-btn'>remove</button>
+      </div>
+      <div>
+        <button className='amount-btn'>
+          <ChevronUp />
+        </button>
+        <p className='amount'>{amount}</p>
+        <button className='amount-btn'>
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
   )
-}
 
+}
+ 
 export default CartItem
--- src/redux/cart/cartItems.js
+++ src/redux/cart/cartItems.js
@@ -1,28 +1,31 @@
 const cartItems = [
-	{
-		id: 1,
-		name: "Samsung Galaxy S7",
-		price: 599.99,
-		quantity: 1,
-		amount : 1,
-		image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png"
-	},
-	{
-		id: 2,
-		name: "google pixel ",
-		price: 499.99,
-		quantity: 1,
-		amount : 3,
-		image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png"
-	},
-	{
-		id: 3,
-		name: "Xiaomi Redmi Note 2",
-		price: 699.99,
-		quantity: 1,
-		amount : 4,
-		image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-1_gvesln.png"
-	},
+  {
+    id: 'rec1JZlfCIBOPdcT2',
+    name: 'Samsung Galaxy S8',
+    price: '399.99',
+    image: 'https://images2.imgbox.com/c2/14/zedmXgs6_o.png',
+    amount: 1,
+  },
+  {
+    id: 'recB6qcHPxb62YJ75',
+    name: 'google pixel',
+    price: '499.99',
+    image: 'https://images2.imgbox.com/fb/3d/O4TPmhlt_o.png',
+    amount: 1,
+  },
+  {
+    id: 'recdRxBsE14Rr2VuJ',
+    name: 'Xiaomi Redmi Note 2',
+    price: '699.99',
+    image: 'https://images2.imgbox.com/4f/3d/WN3GvciF_o.png',
+    amount: 1,
+  },
+  {
+    id: 'recwTo160XST3PIoW',
+    name: 'Samsung Galaxy S7',
+    price: '599.99 ',
+    image: 'https://images2.imgbox.com/2e/7c/yFsJ4Zkb_o.png',
+    amount: 1,
+  },
 ];
-
 export default cartItems;
