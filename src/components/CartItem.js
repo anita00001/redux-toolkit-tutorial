716961b296a2b0c560172187318981b1d135d4c8
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-22 12:44:14 +0545

    Add reducer ACTION for arrow up/down icon

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {ChevronDown, ChevronUp} from '../assets/icons'
-import { removeItem } from '../redux/cart/cartSlice'
+import { removeItem, increase, decrease } from '../redux/cart/cartSlice'
 import { useDispatch } from 'react-redux'
 
 const CartItem = ({id, name, price, amount, image}) => {
@@ -14,11 +14,31 @@ const CartItem = ({id, name, price, amount, image}) => {
         <button className='remove-btn' onClick={()=>{dispatch(removeItem(id))}}>remove</button>
       </div>
       <div>
-        <button className='amount-btn'>
+        <button className='amount-btn'
+        onClick={()=>{
+          dispatch(
+            increase(
+              {id}
+            )
+          )
+        }}
+        >
           <ChevronUp />
         </button>
         <p className='amount'>{amount}</p>
-        <button className='amount-btn'>
+        <button className='amount-btn'
+          onClick={()=>{
+            if(amount === 1) {
+              dispatch(removeItem(id))
+              return
+            }
+            dispatch(
+              decrease(
+                { id }
+              )
+            )
+          }}
+        >
           <ChevronDown />
         </button>
       </div>
