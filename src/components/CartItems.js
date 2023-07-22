c167e444c1ece04e8cb34c85196c852f767ef3e5
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-22 10:13:58 +0545

    Create array of object (dummy data)

---
+++ src/components/CartItems.js
@@ -0,0 +1,28 @@
+const CartItems = [
+	{
+		id: 1,
+		name: "Samsung Galaxy S7",
+		price: 599.99,
+		quantity: 1,
+		amount : 1,
+		image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png"
+	},
+	{
+		id: 2,
+		name: "google pixel ",
+		price: 499.99,
+		quantity: 1,
+		amount : 3,
+		image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png"
+	},
+	{
+		id: 3,
+		name: "Xiaomi Redmi Note 2",
+		price: 699.99,
+		quantity: 1,
+		amount : 4,
+		image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-1_gvesln.png"
+	},
+];
+
+export default CartItems;
